e3ca1b857e603e418508f89ff61deba269dfe67e
Lazar Knezevic <141447582+Lazzzer00@users.noreply.github.com>
2024-01-01 14:40:13 +0100

    Update ScriptComponent.jsx

--- 11. useScript/ScriptComponent.jsx
+++ 11. useScript/ScriptComponent.jsx
@@ -5,8 +5,6 @@ export default function ScriptComponent() {
   const { loading, error } = useScript(
     "https://code.jquery.com/jquery-3.7.1.slim.js"
   );
-  const dots = [" ", ".", "..", "..."];
-  let count = -1;
   if (loading) return <div>Loading...</div>;
 
   if (error) return <div>Error!</div>;
